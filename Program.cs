e098ba7792ee2d4eefba818a013147bce7385e37
ante140102 <none>
2023-03-14 16:30:32 +0100

    NewClass

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 
 namespace ConsoleApp6
 {
+   
     class Program
+        
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
             Console.WriteLine("Vjezba1!");
         }
     }
